2d21101267502e19352e5d829d6a10b720faedd0
Kr1JoVnIk <glinom3s@bk.ru>
2026-09-21 00:29:46 +0300

    Update main file

--- Programming_technologies1.cpp
+++ Programming_technologies1.cpp
@@ -1,20 +1,64 @@
-﻿// Programming_technologies1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
+#include <vector>
+#include <chrono>
+#include <windows.h>
+#include "1klass.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+//  замера времени 
+#define MEASURE_TIME(func_call) \
+    { \
+        auto _t_start = chrono::high_resolution_clock::now(); \
+        func_call; \
+        auto _t_end = chrono::high_resolution_clock::now(); \
+        double _t_duration = chrono::duration<double, milli>(_t_end - _t_start).count(); \
+        cout << "[время выполнения: " << _t_duration << " мс]\n\n"; \
+    }
+
+int main() {
+    
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    LaboratoryWork1 lab;
+    string filename = "war_and_peace2.txt";
+
+    cout << "=== задание 1: подсчет уникальных слов ===\n";
+    MEASURE_TIME(lab.task1_countUniqueWords(filename));
+
+    cout << "=== задание 2: индексация позиций слов ===\n";
+    MEASURE_TIME(lab.task2_indexWordPositions(filename));
+
+    // тестовые данные для заданий 3a-3c
+    vector<int> testVec = { 12, 7, 4, 9, 2, 15, 8, 7, 3, 12, 5, 19, 4, 7 };
+
+    cout << "=== задание 3a: возведение простых чисел в квадрат ===\n";
+    cout << "исходный вектор: ";
+    for (int x : testVec) cout << x << " ";
+    cout << "\n";
+
+    MEASURE_TIME(lab.task3a_squarePrimes(testVec));
+
+    cout << "после обработки: ";
+    for (int x : testVec) cout << x << " ";
+    cout << "\n\n";
+
+    cout << "=== задание 3b: сортировка (нечетные по возр., четные по убыв.) ===\n";
+    MEASURE_TIME(lab.task3b_sortOddAscEvenDesc(testVec));
+
+    cout << "отсортированный вектор: ";
+    for (int x : testVec) cout << x << " ";
+    cout << "\n\n";
+
+    cout << "=== задание 3c: поиск уникальных чисел в диапазоне [5, 12] ===\n";
+    vector<int> rangeResult;
+    MEASURE_TIME(rangeResult = lab.task3c_findUniqueInRange(testVec, 5, 12));
+
+    cout << "уникальные числа в диапазоне: ";
+    for (int x : rangeResult) cout << x << " ";
+    cout << "\n";
+
+    system("pause");
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
